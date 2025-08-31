9107751d720f3422cbb8e0b0be9ce0e244b259ea
Santiago-castilla-coder <santiagojosuecastillacampo@gmail.com>
2025-08-31 10:05:03 -0500

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,12 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const { Pool } = pg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://caseritosapp_db_user:SqSIlNWCymSTTTP3V2vqvCEXDlSOOoze@dpg-d2pu2kfdiees73cchnm0-a.ohio-postgres.render.com/caseritosapp_db',
-  ssl: {
-    rejectUnauthorized: false, // obligatorio para Render
-  },
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // obligatorio para Render
 });
 
 export async function query(text, params) {
