32b7ad269fea9a463725f95327351bdb823c26a8
Santiago-castilla-coder <santiagojosuecastillacampo@gmail.com>
2025-08-31 09:21:18 -0500

    Update index.js
    conection a render

--- server/db/index.js
+++ server/db/index.js
@@ -3,12 +3,19 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 const { Pool } = pg;
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export async function query(text, params){
-  try{
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necesario para Render
+  },
+});
+
+export async function query(text, params) {
+  try {
     const res = await pool.query(text, params);
     return res;
-  }catch(err){
+  } catch (err) {
     console.error('DB QUERY ERROR:', err.message, '\n', { text, params });
     throw err;
   }
